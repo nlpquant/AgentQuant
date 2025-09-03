3a90c05e8e3668f664b8032790e7608d06618507
ciiiii <allen.ccccnm@gmail.com>
2025-09-03 03:31:16 -0700

    Fix hardcode path

--- apps/mcp-server/mcp_server/config.py
+++ apps/mcp-server/mcp_server/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     task_expire: int = int(os.getenv("TASK_EXPIRE", "3600"))  # 1 hour
     data_expire: int = int(os.getenv("DATA_EXPIRE", "43200"))  # 12 hours
 
-    k8s_config_file: str = os.getenv("K8S_CONFIG_FILE", "/Users/cai/github/AgentQuant/deploy/kind/kubeconfig.yaml")
+    k8s_config_file: str = os.getenv("K8S_CONFIG_FILE", "../../deploy/kind/kubeconfig.yaml")
     job_namespace: str = os.getenv("JOB_NAMESPACE", "default")
     job_redis_image: str = os.getenv("JOB_REDIS_IMAGE", "redis:alpine3.22")
     job_redis_host: str = os.getenv("JOB_REDIS_HOST", "host.docker.internal")
